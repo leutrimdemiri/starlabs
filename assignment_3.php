721888fd433fb60013d9fcb9d7ec0c55ab448c80
Leutrim Demiri <leutrimdemiri34@gmail.com>
2021-10-04 17:22:38 +0200

    Update assignment_3.php

--- assignment_3.php
+++ assignment_3.php
@@ -8,26 +8,39 @@
 
 class Caesar
 {
-
-    public function apply_shift($char, $key): string
-    {
-        $output = "";
-
-        for ($i = 0; $i < strlen($char); $i++) {
-
-            // handle capital letters and non-letter characters, but they'll remain capitalized in the output.
-            if (ctype_upper($char[$i]))
-                $output = $output.chr((ord($char[$i]) + $key - 65) % 27 + 65); 
-                                                        // since the space is calculated
-                                                        // we are using 27 letters on modulus operator.
-            else
-                $output = $output.chr((ord($char[$i]) + $key - 97) % 27 + 97);
-            }
-
-        return $output;
+    public $shift;
+    
+    const alphabet = array(
+    "lowercase" => array("a","b","c","d","e","f","g","h","i","j","k",
+    "l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "),
+    
+    "uppercase" => array("A","B","C","D","E","F","G","H","I","J","K",
+    "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"," ")
+    );
+    
+    public function __construct($shift = 0) {
+        $this->shift = $shift % 27;
     }
+    
+    public function encryptCipher($input): string
+    {
+        $result = str_split($input);
+        for ($i = 0; $i < count($result); $i++) {
+            for ($k = 0; $k < 27; $k++) {
+            if ($result[$i] === Caesar::alphabet["lowercase"][$k]) {
+                $result[$i] = Caesar::alphabet["lowercase"][($k + $this->shift) % 27];
+                $k = 27;
+            } elseif ($result[$i] === Caesar::alphabet["uppercase"][$k]) {
+                $result[$i] = Caesar::alphabet["uppercase"][($k + $this->shift) % 27];
+                $k = 27;
+            }
+          }
+        }    
 
+        $result  = implode($result);
+        return $result;
+    }
 }
 
-$test = new Caesar();
-var_dump($test->apply_shift('Star Labs!', 3));
+$output = new Caesar(3);
+var_dump($output->encryptCipher("Star Labs!"));
